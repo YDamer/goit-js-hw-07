08499e78a79690f1d6c4a85964de0dfa9808544b
YDamer <y.damer@ukr.net>
2022-07-01 14:30:11 +0300

    mentor comment fixed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,41 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-console.log(galleryItems);
 
-const galleryImage = document.querySelector(".gallery");
+const galleryContainerEl = document.querySelector(".gallery");
 
-function addGallery(galleryItems) {
-  const markup = galleryItems.map(img =>
-`<div class="gallery__item">
-  <a class="gallery__link" href="${img.original}">
-    <img
-      class="gallery__image"
-      src="${img.preview}"
-      data-source="${img.original}"
-      alt="${img.description}"
-    />
-  </a>
-</div>`).join('');
-  galleryImage.innerHTML = markup
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original.value}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+const onContainerClick = (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("gallery")) return;
+    const source = e.target.dataset.source;
+    
+  const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+
+  instance.show();
 };
-addGallery(galleryItems);
 
-galleryImage.addEventListener("click", handlerGallery);
-
-function handlerGallery (event) {
-  event.preventDefault();
-  const filterDatasetImg = event.target.dataset.source;
-  if (!filterDatasetImg) return;
-
-const instance = basicLightbox.create(`
-<img width="1280" height="860" src="${filterDatasetImg}">`,{
-  onShow: instance => console.log('onShow', instance.element()),
-  onClose: instance => console.log('onClose', instance.element())
-});
-instance.show();
-
-galleryImage.addEventListener("keydown", handlerRemove); 
-function handlerRemove (event) {
-  if (event.code === "Escape") {
-    instance.close(() => galleryImage.removeEventListener("keydown", handlerRemove))}
-}};
+galleryContainerEl.addEventListener("click", onContainerClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
 
-const galleryImage = document.querySelector(".gallery");
-
-function addGallery(galleryItems) {
-  const markup = galleryItems.map(img =>
-`<a class="gallery__item" href="${img.original}">
-<img class="gallery__image" src="${img.preview}" alt="${img.description}" />
-</a>`).join('');
-  galleryImage.innerHTML = markup
-};
-addGallery(galleryItems);
+function createItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<ul class="gallery">
+      <li>
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"          
+          alt="${description}"
+        />
+      </a>
+      </li>
+     
+    </ul>`;
+    })
+    .join("");
+}
 
 const gallery = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   captionsData: 'alt',
 });
-
-
